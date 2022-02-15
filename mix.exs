7438813d93c37e63673367961627ae1053366ede
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-15 09:50:29 -0300

    :heavy_plus_sign: add sobelow

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule PipelineCi.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.14.4", only: :test}
+      {:excoveralls, "~> 0.14.4", only: :test},
+      {:sobelow, "~> 0.11.1", only: :dev}
     ]
   end
 
